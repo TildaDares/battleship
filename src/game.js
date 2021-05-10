97ad470bcf041b4d685505c4a126135ca0502a0b
TildaDares <mathildaudufo@gmail.com>
2021-05-10 12:45:25 +0100

    returns ship if ship Coords includes coord

--- src/game.js
+++ src/game.js
@@ -15,7 +15,28 @@ const Game = (() => {
     return shipCoords;
   };
 
+  const placeShipsOnGrid = (allShipCoords, coord) => {
+    if (allShipCoords.includes(coord)) {
+      return "ship";
+    }
+    return "";
+  };
+
   // const generateHumanGrid = () => {
   //   const humanGridContainer = document.querySelector(".human-grid");
+  //   const allShips = shipCoords(human);
+  //   for (let i = 0; i < 100; i++) {
+  //     const stringCoord = i.toString().padStart(2, 0);
+  //     const uniqId = "hum" + stringCoord;
+  //     let div = document.createElement('div');
+  //     div.classList = 'square '
+  //     grids.push(
+  //       <div
+  //         className={"square " + placeShipsOnGrid(allShips, stringCoord)}
+  //         key={uniqId}
+  //         id={uniqId}
+  //       ></div>
+  //     );
+  //   }
   // };
 })();
